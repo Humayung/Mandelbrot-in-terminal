d364a4169c5d0207d3b0911eea168782fccce86b
cornOblue <humayungmirza@gmail.com>
2020-04-11 04:47:16 +0700

    optimize code

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,12 +1,12 @@
 import os
 import numpy as np
-
+from functools import reduce
 # Output Image Size
 WIDTH = 2.253 * 75 #169
 HEIGHT = 1 * 75 #75
 
 def color(r, g, b):
-     return ('\x1b[48;2;' + str(r) + ';' + str(g) + ';' + str(b) + 'm')
+     return '\x1b[48;2;{0};{1};{2}m'.format(r, g, b)
 
 def is_in_mandelbrot(c):
     z = complex(0, 0)
@@ -18,6 +18,7 @@ def is_in_mandelbrot(c):
             # return '*' # uncomment this line to print it to text
             return color(255, 255, 255) + ' '
     return color(0, 0, iter) + ' ' 
+    
     # return ' ' # uncomment this line to print it to text
 
 def scale(val, s1, e1, s2, e2):
@@ -26,26 +27,15 @@ def scale(val, s1, e1, s2, e2):
     return new_val
 
 
-def drawPoint(x, y):
-        x *= 2
-        x += 1
-        y += 1
-        print("\033[" + str(y) + ";" + str(x) + "H ")
-
 x_range = (-2.25, 0.75)
 y_range = (-1.5, 1.5)
 
 x_scale = np.linspace(x_range[0], x_range[1], int(WIDTH))
 y_scale = np.linspace(y_range[0], y_range[1], int(HEIGHT))
 
-matrix = [[complex(x,y) for x in x_scale] for y in y_scale]
-mandelbrot_plane = [list(map(is_in_mandelbrot, i)) for i in matrix]
-
-board = ""
-for i in mandelbrot_plane:
-    for j in i:
-        board += j
-    board += '\n'
+mandelbrot_plane = [[is_in_mandelbrot(complex(x,y)) for x in x_scale] for y in y_scale]
+stripes = [reduce((lambda x, y: x + y), l) for l in mandelbrot_plane]
+board = reduce((lambda x, y: x + y + '\n'), stripes)
 
 # uncomment these line to print it to text
 # text_file = open('mandelbrot_plane_no_color_{0}x{1}.txt'.format(int(WIDTH), int(HEIGHT)), 'w')
