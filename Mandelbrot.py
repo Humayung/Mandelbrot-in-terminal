3533865e702d3272bb44fa70af637e8844eff78f
cornOblue <humayungmirza@gmail.com>
2020-04-11 04:02:56 +0700

    Comments

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,6 +1,10 @@
 import os
 import numpy as np
-from time import sleep
+
+# Output Image Size
+WIDTH = 2.253 * 75 #169
+HEIGHT = 1 * 75 #75
+
 def color(r, g, b):
      return ('\x1b[48;2;' + str(r) + ';' + str(g) + ';' + str(b) + 'm')
 
@@ -11,10 +15,10 @@ def is_in_mandelbrot(c):
         z = z**2 + c
         iter += 1
         if iter > 25:
-            return '*'
-            # return color(255, 255, 255) + ' '
-    # return color(0, 0, iter) + ' ' 
-    return ' '
+            # return '*' # uncomment this line to print it to text
+            return color(255, 255, 255) + ' '
+    return color(0, 0, iter) + ' ' 
+    # return ' ' # uncomment this line to print it to text
 
 def scale(val, s1, e1, s2, e2):
     percentage = val/(e1 - s1)
@@ -31,9 +35,6 @@ def drawPoint(x, y):
 x_range = (-2.25, 0.75)
 y_range = (-1.5, 1.5)
 
-WIDTH = 169 
-HEIGHT = 75
-
 x_scale = np.linspace(x_range[0], x_range[1], int(WIDTH))
 y_scale = np.linspace(y_range[0], y_range[1], int(HEIGHT))
 
@@ -45,11 +46,13 @@ for i in mandelbrot_plane:
     for j in i:
         board += j
     board += '\n'
-        
-text_file = open('mandelbrot_plane_no_color_{0}x{1}.txt'.format(int(WIDTH), int(HEIGHT)), 'w')
-text_file.write(board)
-# print(board)
-# while True:pass
+
+# uncomment these line to print it to text
+# text_file = open('mandelbrot_plane_no_color_{0}x{1}.txt'.format(int(WIDTH), int(HEIGHT)), 'w')
+# text_file.write(board)
+
+print(board)
+while True:pass
 
 
    
