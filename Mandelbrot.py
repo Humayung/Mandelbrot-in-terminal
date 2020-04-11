e7a46e174eb50a3b50b9ac9e5c5f720ce16ae0eb
cornOblue <humayungmirza@gmail.com>
2020-04-11 23:56:57 +0700

    optimizing

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,9 +1,12 @@
 import os
 import numpy as np
 from functools import reduce
+import math
 # Output Image Size
 WIDTH = 2.253 * 75 #169
 HEIGHT = 1 * 75 #75
+
+on_terminal = True
 
 def color(r, g, b):
      return '\x1b[48;2;{0};{1};{2}m'.format(r, g, b)
@@ -11,39 +14,31 @@ def color(r, g, b):
 def is_in_mandelbrot(c):
     z = complex(0, 0)
     iter = 0
-    while z.imag**2 + z.real**2 < 16:
+    for i in range(256):
         z = z**2 + c
-        iter += 1
-        if iter > 25:
-            # return '*' # uncomment this line to print it to text
-            return color(255, 255, 255) + ' '
-    return color(0, 0, iter) + ' ' 
-    
-    # return ' ' # uncomment this line to print it to text
+        if abs(z) > 2:
+            return color(0, i, i) + ' ' if on_terminal else ' '
+    color_ = int((abs(z) * 255)/2)
+    return color(0, color_,color_ ) + ' ' if on_terminal else '*'
 
-def scale(val, s1, e1, s2, e2):
-    percentage = val/(e1 - s1)
-    new_val = percentage * (e2 - s2) + s2
-    return new_val
+x_bounds = (-2.25, 0.75)
+y_bounds = (-1.5, 1.5)
 
+x_scale = np.linspace(x_bounds[0], x_bounds[1], int(WIDTH))
+y_scale = np.linspace(y_bounds[0], y_bounds[1], int(HEIGHT))
 
-x_range = (-2.25, 0.75)
-y_range = (-1.5, 1.5)
+board = ""
+for y in y_scale:
+    for x in x_scale:
+        board += is_in_mandelbrot(complex(x,y))
+    board += '\n'
 
-x_scale = np.linspace(x_range[0], x_range[1], int(WIDTH))
-y_scale = np.linspace(y_range[0], y_range[1], int(HEIGHT))
-
-mandelbrot_plane = [[is_in_mandelbrot(complex(x,y)) for x in x_scale] for y in y_scale]
-stripes = [reduce((lambda x, y: x + y), l) for l in mandelbrot_plane]
-board = reduce((lambda x, y: x + y + '\n'), stripes)
-
-# uncomment these line to print it to text
-# text_file = open('mandelbrot_plane_no_color_{0}x{1}.txt'.format(int(WIDTH), int(HEIGHT)), 'w')
-# text_file.write(board)
+if not on_terminal:
+    text_file = open('mandelbrot_plane_no_color_{0}x{1}.txt'.format(int(WIDTH), int(HEIGHT)), 'w')
+    text_file.write(board)
 
 print(board)
 while True:pass
-
 
    
 
